868a7d00b26a30b34dc76a291eb0eaa092afb132
Roscoe Clark <roscoe.clark9@gmail.com>
2023-11-28 14:07:15 -0500

    add header no css

--- app/movieHome/page.tsx
+++ app/movieHome/page.tsx
@@ -1,6 +1,11 @@
 import Image from 'next/image'
-
+import Header from '../../components/movieComponents/movieHeader' 
 export default function moiveHome() {
-  return (<h1> moive home page</h1>
+  return (
+    <div>
+    <div> <Header /> </div>
+    <h1> moive home page</h1>
+  </div>
+
   )
 }
---
+++ components/movieComponents/movieHeader.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <div className="navbar">
+                <div className="navbar-start">
+                    <a className="navbar-item">Movie</a>
+                </div>
+                <div className="navbar-end">
+                    <a className="navbar-item">Home</a>
+                    <a className="navbar-item">About</a>
+                    <a className="navbar-item">Contact</a>
+                </div>
+            </div>
+    );
+}
+
+export default Header;
