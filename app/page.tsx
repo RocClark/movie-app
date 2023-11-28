cec8261d34bec5a13b62190fdadd08a9a2ea182f
Roscoe Clark <roscoe.clark9@gmail.com>
2023-11-28 14:06:13 -0500

    add links to pages

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,25 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <div className="flex flex-col gap-2 p-5 max-w-xs w-full dark:bg-slate-800 bg-slate-300 rounded-lg">
-        <div className="text-center my-4">
-          <h1>Movie</h1>
+      <div>
+          <Link
+            href="/movieHome"
+            className="dark:bg-slate-900 bg-slate-400 my-4 p-3 rounded-lg block"
+          >
+            Movie Page
+          </Link>
         </div>
-
         <div>
           <Link
-            href="/"
+            href="/resume"
+            className="dark:bg-slate-900 bg-slate-400 my-4 p-3 rounded-lg block"
+          >
+            Resume
+          </Link>
+        </div>
+        <div>
+          <Link
+            href="/about"
             className="dark:bg-slate-900 bg-slate-400 my-4 p-3 rounded-lg block"
           >
             About
