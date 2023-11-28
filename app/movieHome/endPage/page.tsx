c658e5694bdff1dd4dfef2768bf5d1c19c08d27e
Roscoe Clark <roscoe.clark9@gmail.com>
2023-11-28 14:12:43 -0500

    add movie pages

---
+++ app/movieHome/endPage/page.tsx
@@ -0,0 +1,6 @@
+import Image from 'next/image'
+
+export default function about() {
+  return (<h1> movie page but there is no movie</h1>
+  )
+}
---
+++ app/movieHome/ticketReview/page.tsx
@@ -0,0 +1,6 @@
+import Image from 'next/image'
+
+export default function about() {
+  return (<h1> ticket review and fake pay page</h1>
+  )
+}
