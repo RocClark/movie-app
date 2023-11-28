48a388375ec922c6765d9010fe8e4377f6361727
Roscoe Clark <roscoe.clark9@gmail.com>
2023-11-28 14:06:58 -0500

    add page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
 export default function about() {
-  return (<h1> about</h1>
+  return (<h1> about page</h1>
   )
 }
---
+++ app/resume/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+import React from 'react';
+import Link from 'next/link';
+
+
+export default function resume() {
+  return (<h1> resume page</h1>
+  )
+}
