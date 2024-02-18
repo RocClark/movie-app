12d93361c8765b29f127247b3b9c10f02b996226
Roscoe Clark <roscoe.clark9@gmail.com>
2024-02-18 09:59:31 -0500

    apiv1

--- app/movieHome/page.tsx
+++ app/movieHome/page.tsx
@@ -1,18 +1,19 @@
+'use client'
+
 import Image from 'next/image'
 import Header from '../../components/movieComponents/movieHeader' 
 import Link from 'next/link'
 import MovieList from '../../components/movieComponents/movieList'
+import { useEffect, useState } from 'react'
 
-const MovieData = [
-  {id: 1, title: "The last DJ" },
-  {id: 2, title: "Revenge of the Jocks" },
-  {id: 3, title: "Space Ninja" },
-  {id: 4, title: "Dragon for a Day" },
-  {id: 5, title: "Honey the kids are gone" },
-  {id: 6, title: "Adventure in solfball" }
-]
+export default function MoiveHome() {
+  const [movies, setMovies] = useState([]);
 
-export default function moiveHome() {
+  useEffect(() => {
+    fetch("/api/movies")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
   return (
   <div className='flex flex-wrap items-center justify-center min-h-screen bg-slate-900 '>
     <div className='w-full'>
@@ -20,7 +21,7 @@ export default function moiveHome() {
     </div>
    
     <div className=''>
-      <MovieList movies= {MovieData}/>
+    {movies.length > 0 ? <MovieList movies={movies} /> : "Loading..."}
     </div>
   
 <Link
