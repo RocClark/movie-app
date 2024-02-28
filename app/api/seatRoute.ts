4f41dec0f88d4bb2c79b583a3643e06316bc35d0
Roscoe Clark <roscoe.clark9@gmail.com>
2024-02-28 12:14:05 -0500

    seat api/ debuger added

--- app/api/seatRoute.ts
+++ app/api/seatRoute.ts
@@ -1,11 +1,4 @@
-'use client'
-
-import MoiveScreen from '../../../components/movieComponents/movieScrean'
-import SeatList from '../../../components/movieComponents/seatsList'
-import MovieHeader from '../../../components/movieComponents/movieHeader'
-import { useEffect, useState } from 'react'
-
-
+import { NextResponse } from "next/server";
 
 const SeatData = [
   {id: 1, seatNum: "1a" },
@@ -38,35 +31,12 @@ const SeatData = [
   {id: 38, seatNum: "8c" },
   {id: 39, seatNum: "9c" },
   {id: 30, seatNum: "10c" },
-]
+];
 
+export async function GET(request: Request) {
+  return NextResponse.json(SeatData || []);
+}
 
-
-
-export default function SeatSelect() {
-  const [seat, setSeat] = useState([]);
-
-  useEffect(() => {
-    fetch("/api/seat")
-      .then((response) => response.json())
-      .then((data) => setSeat(data));
-  }, []);
-
-  return (
-  <div>
-     <MovieHeader/>
-    <MoiveScreen/>
-    {/* <SeatList seats= {SeatData}/> */}
-    {seat.length > 0 ? <SeatList seats={seat} /> : "Loading..."}
-
-    
-    <div >
-          <p>show number of seats</p>
-        </div>
-
-    <div >
-          <button>next btn</button>
-        </div>
-  </div>
-  )
+export async function POST(request: Request) {
+  return NextResponse.json({ msg: "comming soon!" });
 }
