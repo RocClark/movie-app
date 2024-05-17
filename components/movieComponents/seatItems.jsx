2a2b1d875e95455c81e21421f86808dded457b20
Roscoe Clark <roscoe.clark9@gmail.com>
2024-05-17 14:04:24 -0400

    add context to seat no change in seat color or status yet

--- components/movieComponents/seatItems.jsx
+++ components/movieComponents/seatItems.jsx
@@ -1,8 +1,13 @@
+import React from "react";
+import { ThemeContext } from "@/components/context";
+
 function SeatItem(props) {
+    const { theme, toggleTheme } = React.useContext(ThemeContext);
     return (
         <li className="mt-3">
            <div>
-          <h3  className="h-10 w-10 rounded-t-full border-red-4 bg-[#33a2d9] text-center ">{props.seatNum}</h3>
+          <h3  className="h-10 w-10 rounded-t-full border-red-4 bg-[#33a2d9] text-center"
+          onClick={toggleTheme}>{props.seatNum}</h3>
         </div>
         
       
