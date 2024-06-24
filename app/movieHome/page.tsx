29525bf48585113b602df254769b271fe42d49f2
Roscoe Clark <roscoe.clark9@gmail.com>
2024-06-24 16:41:20 -0400

    add css to time page and  correted some name

--- app/movieHome/page.tsx
+++ app/movieHome/page.tsx
@@ -3,26 +3,21 @@
 import Image from 'next/image'
 import Header from '../../components/movieComponents/movieHeader' 
 import Link from 'next/link'
-import MovieList from '../../components/movieComponents/movieList'
-import { useEffect, useState } from 'react'
 
 export default function MoiveHome() {
-  const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    fetch("/api/movies")
-      .then((response) => response.json())
-      .then((data) => setMovies(data));
-  }, []);
   return (
   <div className='flex flex-wrap items-center justify-center min-h-screen bg-slate-900 '>
     <div className='w-full'>
        <Header/>
     </div>
    
-    <div className=''>
-    {movies.length > 0 ? <MovieList movies={movies} /> : "Loading..."}
-    </div>
+    <Link
+  href="./movieHome/selectMovie"
+  className="dark:bg-slate-900 bg-slate-400 my-4 p-3 rounded-lg block"
+>
+ Select Movie.
+</Link>
   
 <Link
   href="/"
