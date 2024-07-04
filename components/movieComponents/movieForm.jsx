b11f0d64073f7c10ffff5591049b73addc33385f
Roscoe Clark <roscoe.clark9@gmail.com>
2024-07-04 15:47:03 -0400

    fixed className Mispell

--- components/movieComponents/movieForm.jsx
+++ components/movieComponents/movieForm.jsx
@@ -1,30 +1,30 @@
 import MoiveItem from './movieItem';
 
-function MoiveForm(props) {
+function MoiveForm() {
     return (
 
-<div class="min-w-screen min-h-screen flex items-center justify-center px-5 pb-10 pt-2">
-    <div class="w-full mx-auto rounded-lg bg-white shadow-lg p-5 text-gray-700">
-        <div class="mb-10">
-            <h1 class="text-center font-bold text-xl uppercase">Secure payment info</h1>
+<div className ="min-w-screen min-h-screen flex items-center justify-center px-5 pb-10 pt-2">
+    <div className ="w-full mx-auto rounded-lg bg-white shadow-lg p-5 text-gray-700">
+        <div className ="mb-10">
+            <h1 className ="text-center font-bold text-xl uppercase">Secure payment info</h1>
         </div>
-        <div class="mb-3">
-            <label class="font-bold text-sm mb-2 ml-1">Name on card</label>
+        <div className ="mb-3">
+            <label className ="font-bold text-sm mb-2 ml-1">Name on card</label>
             <div>
-                <input class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="John Smith" type="text"/>
+                <input className ="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="John Smith" type="text"/>
             </div>
         </div>
-        <div class="mb-3">
-            <label class="font-bold text-sm mb-2 ml-1">Card number</label>
+        <div className ="mb-3">
+            <label className ="font-bold text-sm mb-2 ml-1">Card number</label>
             <div>
-                <input class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="0000 0000 0000 0000" type="text"/>
+                <input className ="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="0000 0000 0000 0000" type="text"/>
             </div>
         </div>
-        <div class="mb-3 -mx-2 flex items-end">
-            <div class="px-2 w-2/3">
-                <label class="font-bold text-sm mb-2 ml-1">Expiration date</label>
+        <div className ="mb-3 -mx-2 flex items-end">
+            <div className ="px-2 w-2/3">
+                <label className ="font-bold text-sm mb-2 ml-1">Expiration date</label>
                 <div>
-                    <select class="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
+                    <select className ="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
                         <option value="01">01 - January</option>
                         <option value="02">02 - February</option>
                         <option value="03">03 - March</option>
@@ -40,8 +40,8 @@ function MoiveForm(props) {
                     </select>
                 </div>
             </div>
-            <div class="px-2 w-1/2">
-                <select class="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
+            <div className="px-2 w-1/2">
+                <select className="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
                     <option value="2024">2024</option>
                     <option value="2025">2025</option>
                     <option value="2026">2026</option>
