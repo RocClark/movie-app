2f5e60ed869e5eba8507f28cc36c2a6751bc9aff
Roscoe Clark <roscoe.clark9@gmail.com>
2024-07-16 12:59:37 -0400

    attached movieContext to link

--- components/movieComponents/movieItem.jsx
+++ components/movieComponents/movieItem.jsx
@@ -1,18 +1,24 @@
+import React from 'react';
 import Link from 'next/link';
-function MoiveItem(props) {
+import { PickMovieContext } from '../contexts/pickMovieContext';
+
+  function MoiveItem(props) {
+    const { movie, pickMovie } = React.useContext(PickMovieContext);
+
     return (
-        <li className =" p-6 m-3 rounded-xl bg-slate-400">
-           <div className= "">
+      <li className =" p-6 m-3 rounded-xl bg-slate-400">
+        <div className= "">
           <h3 className="font-serif text-2xl text-center ">{props.title}</h3>
         </div>
         <div className>
           <h3 className="h-36 w-72 m-5 bg-[#313334] rounded-md border-4 border-sky-700">movie poster</h3>
         </div>
-         <div className ="text-center  ">
+        <div className ="text-center  "> 
          <Link
           href="/movieHome/selectTime"
-            className=" sm:px-4 lg:px-8 font-serif text-2xl text-center text-white">
-              Time
+            className=" sm:px-4 lg:px-8 font-serif text-2xl text-center text-white"
+            onClick={pickMovie}>
+              {props.title}
         </Link>
           
          </div>
