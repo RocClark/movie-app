2b91dbd7e6e0a2dc09641517b08637ef9c223888
Roscoe Clark <roscoe.clark9@gmail.com>
2024-07-16 12:44:33 -0400

    attached seatCcontext to the seat icons

--- components/movieComponents/seatItems.jsx
+++ components/movieComponents/seatItems.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { ThemeContext } from "@/components/context";
+import { SeatsContext } from "../contexts/pickSeatContext";
 
 function SeatItem(props) {
     const { theme, toggleTheme } = React.useContext(ThemeContext);
+    const { seats, pickSeats } = React.useContext(SeatsContext);
     return (
         <li className="mt-3">
            <div>
           <h3  className="h-10 w-10 rounded-t-full border-red-4 bg-[#33a2d9] text-center"
-          onClick={toggleTheme}>{props.seatNum}</h3>
+          onClick={pickSeats}>{props.seatNum}</h3>
         </div>
         
       
