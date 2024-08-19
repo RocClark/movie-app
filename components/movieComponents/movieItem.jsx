95ae7946224555d3c9d17b4d6567955320a79d4e
Roscoe Clark <roscoe.clark9@gmail.com>
2024-08-19 12:15:04 -0400

    can a sign a movie to the ticket

--- components/movieComponents/movieItem.jsx
+++ components/movieComponents/movieItem.jsx
@@ -1,9 +1,18 @@
+"use client";
 import React from 'react';
 import Link from 'next/link';
 import { PickMovieContext } from '../contexts/pickMovieContext';
+import {TicketReviewContext } from '../contexts/ticketContext';
+
 
   function MoiveItem(props) {
-    const { movie, pickMovie } = React.useContext(PickMovieContext);
+   // const { movie, pickMovie } = React.useContext(PickMovieContext);
+    const { ticket, pickMovie } = React.useContext(TicketReviewContext);
+
+    const handlePickMovie = () => {
+      pickMovie(props.title); // This will update the movie property in the ticket object
+    };
+  
 
     return (
       <li className =" p-6 m-3 rounded-xl bg-slate-400">
@@ -17,7 +26,7 @@ import { PickMovieContext } from '../contexts/pickMovieContext';
          <Link
           href="/movieHome/selectTime"
             className=" sm:px-4 lg:px-8 font-serif text-2xl text-center text-white"
-            onClick={pickMovie}>
+            onClick={handlePickMovie}>
               {props.title}
         </Link>
           
