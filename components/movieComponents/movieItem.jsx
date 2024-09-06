3144f0aa8c06ee966b741c1528c11fe42f66b341
Roscoe Clark <roscoe.clark9@gmail.com>
2024-09-06 10:28:37 -0400

    fix alinment issue

--- components/movieComponents/movieItem.jsx
+++ components/movieComponents/movieItem.jsx
@@ -15,12 +15,14 @@ import {TicketReviewContext } from '../contexts/ticketContext';
   
 
     return (
-      <li className =" p-6 m-3 rounded-xl bg-slate-400">
+      <li className="p-4 m-4 rounded-xl bg-slate-400">
         <div >
           <h3 className="font-serif text-2xl text-center ">{props.title}</h3>
         </div>
-        <div >
-          <h3 className="h-36 w-72 m-5 bg-[#313334] rounded-md border-4 border-sky-700">movie poster</h3>
+        <div className="flex justify-center items-center">
+          <h3 className="h-36 w-72 m-5 bg-[#313334] rounded-md border-4 border-sky-700">
+            movie poster
+          </h3>
         </div>
         <div className ="text-center  "> 
          <Link
